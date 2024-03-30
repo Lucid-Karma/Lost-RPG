73eade9874954f5094740fcc71f8f7416155573e
Beyzanur Ozen <beyzaalways@gmail.com>
2024-03-30 15:57:34 +0300

    Persistent Objects Between Scenes

--- Lost/Assets/Project/[GAME]/Scripts/Other/DDOL.cs
+++ Lost/Assets/Project/[GAME]/Scripts/Other/DDOL.cs
@@ -2,12 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DDOL : MonoBehaviour
+public class DDOL<T> : MonoBehaviour where T : DDOL<T>
 {
-    public List<GameObject> DontDestroyObjects = new();
+    public static T Instance { get; private set; }
+    public static T DDOLObject = null;
 
-    private void Start()
+    void Awake()
     {
-        DontDestroyOnLoad(gameObject);
+        if (DDOLObject == null)
+        {
+            DDOLObject = (T)this;
+            DontDestroyOnLoad(this);
+        }
+        else if (this != DDOLObject)
+        {
+            Destroy(gameObject);
+        }
     }
 }
+
+//public class DDOL : MonoBehaviour 
+//{
+//    public static DDOL Instance { get; private set; }
+//    public static DDOL DDOLObject = null;
+
+//    void Awake()
+//    {
+//        if (DDOLObject == null)
+//        {
+//            DDOLObject = this;
+//            DontDestroyOnLoad(this);
+//        }
+//        else if (this != DDOLObject)
+//        {
+//            Destroy(gameObject);
+//        }
+//    }
+//}
